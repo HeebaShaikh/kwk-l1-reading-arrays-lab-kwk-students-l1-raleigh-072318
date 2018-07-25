3efbeae15b9bb4f32111143d10159a21b8f35741
HeebaShaikh <heeba126@gmail.com>
2018-07-25 17:38:48 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -7,7 +7,7 @@ STUDENT_NAMES = [
   "Rihanna"
 ]
 
-puts STUDENT_NAMES[4]
+puts STUDENT_NAMES[0, 1, 2, 3, 4, 5]
 
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
