9131c2a9cc8d31385f89539ddf347a28ad57cef7
HeebaShaikh <heeba126@gmail.com>
2018-07-25 17:44:36 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -9,18 +9,21 @@ STUDENT_NAMES = [
 
 puts STUDENT_NAMES[5]
 puts STUDENT_NAMES[4]
-puts STUDENT_NAMES[0]
+
 
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
+  puts STUDENT_NAMES[0]
 end
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
+  puts STUDENT_NAMES[4]
 end
 
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
+  puts STUDENT_NAMES[5]
 end
 
 def first_student_by_method
